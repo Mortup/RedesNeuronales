0585355243a71cf04fc130f18970fce6b6e1b132
Mortup <g.uribemontero@gmail.com>
2018-10-31 01:00:02 -0300

    Training with dataset ready

--- neural/main.py
+++ neural/main.py
@@ -10,25 +10,55 @@ def getDataSet():
     f = open('wine.data', 'r')
     lines = f.readlines()
 
-    result = []
+    result = [[],[]]
 
     for line in lines:
         splitted = line.split(',')
+
+        for i in range(len(splitted)):
+            if i == 0:
+                splitted[i] = int(splitted[i])
+            else:
+                splitted[i] = float(splitted[i])
+
         wineType = getResultArray(splitted[0])
         wineProperties = splitted[1:]
-        result.append([wineProperties, wineType])
+        result[0].append(wineProperties)
+        result[1].append(wineType)
 
-    for a in result:
-        print a
     return result
+
+def normalize_dataset(original):
+    for i in range(len(original[0])):
+        aMin = None
+        aMax = None
+
+        for j in range(len(original)):
+            v = original[j][i]
+            if (aMin is None) or (v < aMin):
+                aMin = v
+            if (aMax is None) or (v > aMax):
+                aMax = v
+
+        for j in range(len(original)):
+            v = original[j][i]
+            original[j][i] = (v - aMin)/(aMax - aMin)
+
+    return original
 
 
 if __name__ == "__main__":
-    getDataSet()
-    n = Network(2, [2,3,3,2])
-    nEpochs = 100000
+    raw_dataset = getDataSet()
+    training_set = [normalize_dataset(raw_dataset[0]), raw_dataset[1]]
+
+    n = Network(13, [7,5,5,3])
+    nEpochs = 10000
 
     for i in xrange(nEpochs):
-        n.epoch([[0,0],[1,0],[0,1],[1,1]],[[0,1],[1,0],[1,0],[0,1]])
-        showProgress(i,nEpochs,"Training")
+        n.epoch(training_set[0], training_set[1])
+        showProgress(i, nEpochs)
     print("\nEntrenamiento terminado!")
+
+    print(n.feed(normalize_dataset(raw_dataset[0])[2]))
+    print(n.feed(normalize_dataset(raw_dataset[0])[103]))
+    print(n.feed(normalize_dataset(raw_dataset[0])[161]))
