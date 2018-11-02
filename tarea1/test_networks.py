8860aa0ea3f75ba52e4599f80e487c50cab22233
Mortup <g.uribemontero@gmail.com>
2018-11-02 02:31:44 -0300

    Added unit tests

--- tarea1/test_networks.py
+++ tarea1/test_networks.py
@@ -1,20 +1,31 @@
 import math
+import random
 
 import unittest
 
 import network
-class TestSingleNeuronLayer(unittest.TestCase):
+class TestNetworks(unittest.TestCase):
 
-    def test_neutral_layer(self):
-        layer = network.Layer(1, [1,1])
-        self.assertEquals(layer.feed([0,0]), [0.5])
-        self.assertEquals(layer.feed([1,1]), [float(1)/(1+math.exp(2))])
+    def test_case_1(self):
+        random.seed(1313)
+        network.learningRate = 0.6
 
-    def test_weighted_layer(self):
-        layer = network.Layer(1, [3,2])
-        self.assertEquals(layer.feed([0,0]), [0.5])
-        self.assertEquals(layer.feed([1,1]), [float(1)/(1+math.exp(5))])
-        self.assertEquals(layer.feed([2,3]), [float(1)/(1+math.exp(12))])
+        n = network.Network(2, [1,1])
+        neuron1 = n.layers[0].neurons[0]
+        neuron1.bias = 0.5
+        neuron1.weights = [0.4, 0.3]
+
+        neuron2 = n.layers[1].neurons[0]
+        neuron2.bias = 0.4
+        neuron2.weight = [0.3]
+
+        n.train([1,1],[1])
+
+        self.assertEquals(neuron1.bias,0.5027423114929557)
+        self.assertEquals(neuron1.weights, [0.40274231149295564, 0.3027423114929556])
+
+        self.assertEquals(neuron2.bias,0.44604768753008517)
+        self.assertEquals(neuron2.weights, [0.3701588276492822])
 
 
 if __name__ == '__main__':
--- tarea1/test_neurons.py
+++ tarea1/test_neurons.py
@@ -7,52 +7,52 @@ class TestNeuronMethods(unittest.TestCase):
 
     def test_one_input(self):
         zero_neuron = neurons.Neuron([0], 1)
-        self.assertEqual(zero_neuron.get_raw_feed([1]), 0)
-        self.assertEqual(zero_neuron.get_raw_feed([0]), 0)
-        self.assertEqual(zero_neuron.get_raw_feed([42]), 0)
+        self.assertEqual(zero_neuron.get_weighted_input([1]), 0)
+        self.assertEqual(zero_neuron.get_weighted_input([0]), 0)
+        self.assertEqual(zero_neuron.get_weighted_input([42]), 0)
 
         neutral_neuron = neurons.Neuron([1], 1)
-        self.assertEqual(neutral_neuron.get_raw_feed([1]), 1)
-        self.assertEqual(neutral_neuron.get_raw_feed([0]), 0)
-        self.assertEqual(neutral_neuron.get_raw_feed([42]), 42)
-        self.assertEqual(neutral_neuron.get_raw_feed([0.3]), 0.3)
+        self.assertEqual(neutral_neuron.get_weighted_input([1]), 1)
+        self.assertEqual(neutral_neuron.get_weighted_input([0]), 0)
+        self.assertEqual(neutral_neuron.get_weighted_input([42]), 42)
+        self.assertEqual(neutral_neuron.get_weighted_input([0.3]), 0.3)
 
         weighted_neuron = neurons.Neuron([0.4], 1)
-        self.assertEqual(weighted_neuron.get_raw_feed([1]), 0.4)
-        self.assertEqual(weighted_neuron.get_raw_feed([0]), 0)
-        self.assertEqual(weighted_neuron.get_raw_feed([42]), 42*0.4)
-        self.assertEqual(weighted_neuron.get_raw_feed([0.3]), 0.3*0.4)
+        self.assertEqual(weighted_neuron.get_weighted_input([1]), 0.4)
+        self.assertEqual(weighted_neuron.get_weighted_input([0]), 0)
+        self.assertEqual(weighted_neuron.get_weighted_input([42]), 42*0.4)
+        self.assertEqual(weighted_neuron.get_weighted_input([0.3]), 0.3*0.4)
 
     def test_zero_neuron(self):
         zero_n_3 = neurons.Neuron([0,0,0], 0.5)
-        self.assertEqual(zero_n_3.get_raw_feed([1,1,1]), 0)
-        self.assertEqual(zero_n_3.get_raw_feed([0,0,0]), 0)
-        self.assertEqual(zero_n_3.get_raw_feed([42,130,21]), 0)
+        self.assertEqual(zero_n_3.get_weighted_input([1,1,1]), 0)
+        self.assertEqual(zero_n_3.get_weighted_input([0,0,0]), 0)
+        self.assertEqual(zero_n_3.get_weighted_input([42,130,21]), 0)
 
         zero_n_5 = neurons.Neuron([0,0,0,0,0], 0.5)
-        self.assertEqual(zero_n_5.get_raw_feed([1,1,1,1,1]), 0)
-        self.assertEqual(zero_n_5.get_raw_feed([0,0,0,0,0]), 0)
-        self.assertEqual(zero_n_5.get_raw_feed([42,130,21,12,9]), 0)
+        self.assertEqual(zero_n_5.get_weighted_input([1,1,1,1,1]), 0)
+        self.assertEqual(zero_n_5.get_weighted_input([0,0,0,0,0]), 0)
+        self.assertEqual(zero_n_5.get_weighted_input([42,130,21,12,9]), 0)
 
     def test_neutral_neuron(self):
         neutral_n_3 = neurons.Neuron([1,1,1], 0.5)
-        self.assertEqual(neutral_n_3.get_raw_feed([1,1,1]), 3)
-        self.assertEqual(neutral_n_3.get_raw_feed([0,0,0]), 0)
-        self.assertEqual(neutral_n_3.get_raw_feed([42,130,21]), 193)
+        self.assertEqual(neutral_n_3.get_weighted_input([1,1,1]), 3)
+        self.assertEqual(neutral_n_3.get_weighted_input([0,0,0]), 0)
+        self.assertEqual(neutral_n_3.get_weighted_input([42,130,21]), 193)
 
         neutral_n_5 = neurons.Neuron([1,1,1,1,1], 0.5)
-        self.assertEqual(neutral_n_5.get_raw_feed([1,1,1,1,1]), 5)
-        self.assertEqual(neutral_n_5.get_raw_feed([0,0,0,0,0]), 0)
-        self.assertEqual(neutral_n_5.get_raw_feed([4,10,21,3,4]), 42)
+        self.assertEqual(neutral_n_5.get_weighted_input([1,1,1,1,1]), 5)
+        self.assertEqual(neutral_n_5.get_weighted_input([0,0,0,0,0]), 0)
+        self.assertEqual(neutral_n_5.get_weighted_input([4,10,21,3,4]), 42)
 
     def test_bias_should_not_affect_raw_result(self):
         neuron1 = neurons.Neuron([1,2], 1)
         neuron2 = neurons.Neuron([1,2], 5)
         neuron3 = neurons.Neuron([1,2], 15)
         
-        self.assertEqual(neuron1.get_raw_feed([2,4]), neuron2.get_raw_feed([2,4]))
-        self.assertEqual(neuron1.get_raw_feed([2,4]), neuron3.get_raw_feed([2,4]))
-        self.assertEqual(neuron2.get_raw_feed([2,4]), neuron3.get_raw_feed([2,4]))
+        self.assertEqual(neuron1.get_weighted_input([2,4]), neuron2.get_weighted_input([2,4]))
+        self.assertEqual(neuron1.get_weighted_input([2,4]), neuron3.get_weighted_input([2,4]))
+        self.assertEqual(neuron2.get_weighted_input([2,4]), neuron3.get_weighted_input([2,4]))
 
 class TestPerceptronMethods(unittest.TestCase):
 
@@ -86,15 +86,15 @@ class TestSigmoidMethods(unittest.TestCase):
 
     def test_neutral_sigmoid(self):
         sigmoid = neurons.Sigmoid([0,0,0],1)
-        self.assertEqual(sigmoid.feed([0,0,0]),0.5)
-        self.assertEqual(sigmoid.feed([1,1,1]),0.5)
-        self.assertEqual(sigmoid.feed([42,420,1]),0.5)
+        self.assertEqual(sigmoid.feed([0,0,0]),0.7310585786300049)
+        self.assertEqual(sigmoid.feed([1,1,1]),0.7310585786300049)
+        self.assertEqual(sigmoid.feed([42,420,1]),0.7310585786300049)
 
     def test_sigmoid_result(self):
         sigmoid = neurons.Sigmoid([1,2,3],1)
-        self.assertEqual(sigmoid.feed([0,0,0]),0.5)
-        self.assertEqual(sigmoid.feed([1,0,0]),float(1)/(1+math.exp(1)))
-        self.assertEqual(sigmoid.feed([10,20,5]),float(1)/(1+math.exp(65)))
+        self.assertEqual(sigmoid.feed([0,0,0]),0.7310585786300049)
+        self.assertEqual(sigmoid.feed([1,0,0]),0.8807970779778823)
+        self.assertEqual(sigmoid.feed([10,20,5]),1.0)
 
 if __name__ == '__main__':
     unittest.main()
